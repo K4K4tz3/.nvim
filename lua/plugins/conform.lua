ccca68e4016caf1828721e3fe01ba1eb4801a28d
K4K4tz3 <wolff.joshua2002@icloud.com>
2026-07-26 19:00:41 +0200

    [feat]: added conform for c++ formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+	"stevearc/conform.nvim",
+
+	opts = {
+		formatters_by_ft = {
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+		},
+
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
+	},
+}
