05546f80f22495905c6c454c849936c8bf3c99bd
K4K4tz3 <wolff.joshua2002@icloud.com>
2026-09-16 15:33:17 +0200

    [feat(overseer)]: added unit_test for scons

---
+++ lua/overseer/template/user/unit_test.lua
@@ -0,0 +1,15 @@
+return {
+  name = "Run tests",
+  desc = "Run C++ unit tests",
+
+  builder = function()
+    return {
+      cmd = "scons bin/tests debug_symbols=yes optimize=none && bin/tests",
+      cwd = vim.fn.getcwd(),
+
+      components = {
+        "default",
+      },
+    }
+  end,
+}
